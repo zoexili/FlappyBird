b59589f92c88e2ff6133fe44f1331eaac72c8a34
lixi <lxi1@binghamton.edu>
2024-01-15 20:11:43 -0500

    Added c# scripts and sprite pngs

---
+++ Assets/PipeSpawnScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnScript : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnRate = 3;
+    private float timer = 0;
+    public float heightOffset = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            spawn();
+            timer = 0;
+        }
+    }
+
+    void spawn()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+    }
+}
